ca33d2bec3b63dc2aa2a00b4c9475f26723a4adb
RamyaU <ramyayadav2016@gmail.com>
2024-08-15 14:51:23 +0530

    simple jsx code rendering

--- taskmate/src/App.js
+++ taskmate/src/App.js
@@ -7,12 +7,49 @@
 //     )
 // }
 
-//second way \
-const App = () =>
-    {
-        return (
-            <h1>Hi!</h1>
-        )
-    }
+// //second way \
+// const App = () =>
+//     {
+//         return (
+//             <h1>Hi!</h1>
+//         )
+//     }
 
-export default App;
+// export default App;
+
+// //function based component
+// import React from 'react'
+
+// export default function App() {
+//   return (
+//     <div>
+      
+//     </div>
+//   )
+// }
+
+//arrow function component
+// import React from 'react'
+
+// const App = () => {
+//   return (
+//     <div>
+      
+//     </div>
+//   )
+// }
+
+// export default App
+
+//class based component
+import React, { Component } from 'react'
+
+export default class App extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Ramya is teh best</h1>
+      </div>
+    )
+  }
+}

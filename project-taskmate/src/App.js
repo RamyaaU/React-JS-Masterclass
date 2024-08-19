a34aa67da1b142127ab8004d4e0ebba3d5f3bfc8
RamyaU <ramyayadav2016@gmail.com>
2024-08-19 17:14:13 +0530

    finally local storage setup

--- project-taskmate/src/App.js
+++ project-taskmate/src/App.js
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import './App.css';
 import Header from './components/Header';
 import AddTask from '../src/components/AddTask';
@@ -7,7 +7,25 @@ import ShowTask from '../src/components/ShowTask';
 function App() {
 
   //Initialize an empty array as the initial task list
-  const [tasklist,setTasklist] = useState([]);
+  //const [tasklist,setTasklist] = useState(JSON.parse(localStorage.getItem("tasklist")) || []);
+  const [tasklist, setTasklist] = useState(() => {
+    try {
+      //fetches the tasks from local storage 
+      const savedTasklist = localStorage.getItem("tasklist");
+      //if tasks found returns the task else it will return empty array
+      return savedTasklist ? JSON.parse(savedTasklist) : [];
+    } catch (error) {
+      console.error("Error parsing tasklist from localStorage:", error);
+      return []; // Fallback to an empty array
+    }
+  });
+
+  //using use effect whenevr the task list chnages teh useeffect will save the
+  //latest to browsers local storage 
+  useEffect(() => {
+    localStorage.setItem("tasklist", JSON.stringify(tasklist))
+  }, [tasklist]);
+  
   const [task, setTask] = useState([]);
 
   return (

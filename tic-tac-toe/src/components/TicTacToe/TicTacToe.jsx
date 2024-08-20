1eb4ce817f62753781c674c59ad8f118e45f462a
RamyaU <ramyayadav2016@gmail.com>
2024-08-20 21:49:50 +0530

    app styling

--- tic-tac-toe/src/components/TicTacToe/TicTacToe.jsx
+++ tic-tac-toe/src/components/TicTacToe/TicTacToe.jsx
@@ -1,18 +1,22 @@
-import React from 'react'
-// import TicTacToe from './TicTacToe.css'
-import circleicon from '../assets/circle.png'
-import crossicon from '../assets/cross.png'
+import React from 'react';
+import './TicTacToe.css'; // Correctly import the CSS file
+import circleicon from '../assets/circle.png';
+import crossicon from '../assets/cross.png';
 
 const TicTacToe = () => {
   return (
     <div className='container'>
-      <h1 className="title">Tic Tac Toe Game In <span>React</span></h1>
-     <div className='board'>
-
-     </div>
-     <button className='reset'>Reset</button>
+      <h1 className='title'>Tic Tac Toe Game In <span>React</span></h1>
+      <div className='board'>
+        <div className='row1'>
+          <div className='boxes'> </div>
+          <div className='boxes'></div>
+          <div className='boxes'></div>
+        </div>
+      </div>
+      <button className='reset'>Reset</button>
     </div>
-  )
+  );
 }
 
-export default TicTacToe
+export default TicTacToe;

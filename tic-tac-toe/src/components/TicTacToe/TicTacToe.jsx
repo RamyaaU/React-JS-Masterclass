43474e1484ce6ab70558ea32a6e932816daf3926
RamyaU <ramyayadav2016@gmail.com>
2024-08-20 21:34:10 +0530

    created component tic tac toe and added assets

---
+++ tic-tac-toe/src/components/TicTacToe/TicTacToe.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import TicTacToe from './TicTacToe.css'
+
+const TicTacToe = () => {
+  return (
+    <div>
+      <TicTacToe />
+    </div>
+  )
+}
+
+export default TicTacToe

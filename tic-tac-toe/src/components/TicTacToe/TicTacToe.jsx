1db29bb4d743c63fa8bad4afbfa7c778bbef9c46
RamyaU <ramyayadav2016@gmail.com>
2024-08-20 21:55:40 +0530

    complete css branding

--- tic-tac-toe/src/components/TicTacToe/TicTacToe.jsx
+++ tic-tac-toe/src/components/TicTacToe/TicTacToe.jsx
@@ -13,6 +13,16 @@ const TicTacToe = () => {
           <div className='boxes'></div>
           <div className='boxes'></div>
         </div>
+        <div className='row2'>
+          <div className='boxes'> </div>
+          <div className='boxes'></div>
+          <div className='boxes'></div>
+        </div>
+        <div className='row3'>
+          <div className='boxes'> </div>
+          <div className='boxes'></div>
+          <div className='boxes'></div>
+        </div>
       </div>
       <button className='reset'>Reset</button>
     </div>

578ac740d011ab0eb90b9ede26612c127529bf64
RamyaU <ramyayadav2016@gmail.com>
2024-08-20 22:19:27 +0530

    logic change for tic tac toe and css rebranding

--- tic-tac-toe/src/components/TicTacToe/TicTacToe.jsx
+++ tic-tac-toe/src/components/TicTacToe/TicTacToe.jsx
@@ -3,65 +3,85 @@ import './TicTacToe.css';
 import circleicon from '../assets/circle.png';
 import crossicon from '../assets/cross.png';
 
-// empty array
-let data = ["", "", "", "", "", "", "", "", ""];
-let x = "X";
-let o = "O";
-
 const TicTacToe = () => {
-  let [count, setCount] = useState(0);
-  let [lock, setLock] = useState(false);
+  const initialBoard = Array(9).fill(null);
+  const [board, setBoard] = useState(initialBoard);
+  const [isXNext, setIsXNext] = useState(true);
+  const [winner, setWinner] = useState(null);
 
-  const toggle = (e, num) => {
-    if (lock) {
-      return;
-    }
-    if (data[num] !== "") {
-      return; // Prevent overwriting an already filled box
-    }
+  const handleClick = (index) => {
+    if (board[index] || winner) return; // Prevent overriding and playing after game ends
 
-    if (count % 2 === 0) {
-      e.target.innerHTML = `<img src='${crossicon}' alt='X'>`;
-      data[num] = x;
-    } else {
-      e.target.innerHTML = `<img src='${circleicon}' alt='O'>`;
-      data[num] = o;
+    const newBoard = [...board];
+    newBoard[index] = isXNext ? 'X' : 'O';
+    setBoard(newBoard);
+    setIsXNext(!isXNext);
+
+    const winningPlayer = calculateWinner(newBoard);
+    if (winningPlayer) {
+      setWinner(winningPlayer);
     }
-    setCount(count + 1);
-  }
+  };
+
+  const calculateWinner = (squares) => {
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+    for (let i = 0; i < lines.length; i++) {
+      const [a, b, c] = lines[i];
+      if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+        return squares[a];
+      }
+    }
+    return null;
+  };
+
+  const renderSquare = (index) => {
+    const icon = board[index] === 'X' ? crossicon : board[index] === 'O' ? circleicon : null;
+    return (
+      <div className='boxes' onClick={() => handleClick(index)}>
+        {icon && <img src={icon} alt={board[index]} />}
+      </div>
+    );
+  };
 
   const resetGame = () => {
-    data = ["", "", "", "", "", "", "", "", ""];
-    setCount(0);
-    setLock(false);
-    document.querySelectorAll('.boxes').forEach(box => {
-      box.innerHTML = "";
-    });
-  }
+    setBoard(initialBoard);
+    setIsXNext(true);
+    setWinner(null);
+  };
 
   return (
     <div className='container'>
       <h1 className='title'>Tic Tac Toe Game In <span>React</span></h1>
       <div className='board'>
         <div className='row1'>
-          <div className='boxes' onClick={(e) => toggle(e, 0)}></div>
-          <div className='boxes' onClick={(e) => toggle(e, 1)}></div>
-          <div className='boxes' onClick={(e) => toggle(e, 2)}></div>
+          {renderSquare(0)}
+          {renderSquare(1)}
+          {renderSquare(2)}
         </div>
         <div className='row2'>
-          <div className='boxes' onClick={(e) => toggle(e, 3)}></div>
-          <div className='boxes' onClick={(e) => toggle(e, 4)}></div>
-          <div className='boxes' onClick={(e) => toggle(e, 5)}></div>
+          {renderSquare(3)}
+          {renderSquare(4)}
+          {renderSquare(5)}
         </div>
         <div className='row3'>
-          <div className='boxes' onClick={(e) => toggle(e, 6)}></div>
-          <div className='boxes' onClick={(e) => toggle(e, 7)}></div>
-          <div className='boxes' onClick={(e) => toggle(e, 8)}></div>
+          {renderSquare(6)}
+          {renderSquare(7)}
+          {renderSquare(8)}
         </div>
       </div>
       <button className='reset' onClick={resetGame}>Reset</button>
+      {winner && <h2 className='winner'>{`Winner: ${winner}`}</h2>}
     </div>
   );
-}
+};
 
 export default TicTacToe;

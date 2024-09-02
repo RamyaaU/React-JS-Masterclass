1c5979cfbe91a7fa42815372ea9111b8cb9f633b
RamyaU <ramyayadav2016@gmail.com>
2024-09-02 12:51:23 +0530

    created a component and rendered in app.js

---
+++ tasktrek/tasktrek/src/components/Card.jsx
@@ -0,0 +1,20 @@
+// import React from "react";
+
+// function Card()
+// {
+//     return <h1>Card components</h1>
+// }
+
+// export default Card;
+
+import React from 'react'
+
+const Card = () => {
+  return (
+    <div>
+       <h1>Card components</h1>
+    </div>
+  )
+}
+
+export default Card

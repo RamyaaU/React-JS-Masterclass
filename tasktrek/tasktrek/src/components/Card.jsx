69ab39b80bd509f46111ec4180a59d1bcc7afdcd
RamyaU <ramyayadav2016@gmail.com>
2024-09-03 12:08:37 +0530

    moved add task code from card to create to do

--- tasktrek/tasktrek/src/components/Card.jsx
+++ tasktrek/tasktrek/src/components/Card.jsx
@@ -1,19 +1,11 @@
 import React from 'react'
 
 const Card = () => {
-    
-  const tasks = 1;
-  const hideButton =  true
-  const countTasks = () =>
-  {
-    return tasks === 0 ?  "No tasks available " : `Tasks : ${tasks}`
-  }
-
+     
   return (
-     <React.Fragment>
-       <h1> {countTasks()} </h1>
-       <button>Add Task</button>
-    </React.Fragment>
+    <div>
+
+    </div>
   )
 }
 
--- tasktrek/tasktrek/src/components/CreateToDo.jsx
+++ tasktrek/tasktrek/src/components/CreateToDo.jsx
@@ -1,11 +1,20 @@
 import React from "react";
 
 const CreateToDo = () => {
+
+    const tasks = 1;
+    const hideButton =  true
+    const countTasks = () =>
+    {
+      return tasks === 0 ?  "No tasks available " : `Tasks : ${tasks}`
+    }
+
+    
     return (
-    <div>
-            <h1>Create new To Do from here</h1>
-                <button>Add Task </button>
-        </div>
+        <React.Fragment>
+        <h1> {countTasks()} </h1>
+        <button>Add Task</button>
+     </React.Fragment>
     )
 }
 

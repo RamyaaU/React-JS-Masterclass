8a2b28116451a09a456278cec490f54f16403e6f
RamyaU <ramyayadav2016@gmail.com>
2024-09-03 12:05:17 +0530

    created a new component and rendered in app component

--- tasktrek/tasktrek/src/App.js
+++ tasktrek/tasktrek/src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import Card from "./components/Card.jsx";
+import CreateToDo from './components/CreateToDo.jsx';
 
 function App() {
   return (
     <div className="App">
       <header>
         <Card /> 
+        <CreateToDo />
       </header>
     </div>
   );
---
+++ tasktrek/tasktrek/src/components/CreateToDo.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const CreateToDo = () => {
+    return (
+    <div>
+            <h1>Create new To Do from here</h1>
+                <button>Add Task </button>
+        </div>
+    )
+}
+
+export default CreateToDo;

d26bcf1d0cd7b0a79afac91422d99ad16dffd00c
RamyaU <ramyayadav2016@gmail.com>
2024-09-03 11:53:29 +0530

    adding commit

--- tasktrek/tasktrek/src/components/Card.jsx
+++ tasktrek/tasktrek/src/components/Card.jsx
@@ -2,11 +2,16 @@ import React from 'react'
 
 const Card = () => {
     
-  const tasks = 5;
+  const tasks = 1;
+  const hideButton =  true
+  const countTasks = () =>
+  {
+    return tasks === 0 ?  "No tasks available " : `Tasks : ${tasks}`
+  }
 
   return (
      <React.Fragment>
-       <h1>Card components</h1>
+       <h1> {countTasks()} </h1>
        <button>Add Task</button>
     </React.Fragment>
   )

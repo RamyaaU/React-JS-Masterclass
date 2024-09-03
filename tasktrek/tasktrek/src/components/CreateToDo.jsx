b5b4b065a1df1f9e5358844f3d6ec8883aa2045e
RamyaU <ramyayadav2016@gmail.com>
2024-09-03 12:38:54 +0530

    defining an element

--- tasktrek/tasktrek/src/components/CreateToDo.jsx
+++ tasktrek/tasktrek/src/components/CreateToDo.jsx
@@ -4,16 +4,18 @@ const CreateToDo = () => {
 
     const tasks = 1;
     const hideButton =  true
+    // const styles =  {
+    //     backgroundColor : "red"
+    // }
     const countTasks = () =>
     {
       return tasks === 0 ?  "No tasks available " : `Tasks : ${tasks}`
     }
-
     
     return (
         <React.Fragment>
         <h1> {countTasks()} </h1>
-        <button>Add Task</button>
+        <button  disabled={hideButton} value="Add task button">Add Task</button>
      </React.Fragment>
     )
 }

2e3b5046a5cf506497966094139ef3e7549a84bc
RamyaU <ramyayadav2016@gmail.com>
2024-09-03 14:00:53 +0530

    handling use state hook

--- tasktrek/tasktrek/src/components/CreateToDo.jsx
+++ tasktrek/tasktrek/src/components/CreateToDo.jsx
@@ -1,7 +1,9 @@
-import React from "react";
+import React , {useState} from "react";
 
 const CreateToDo = () => {
     let tasks = 0;
+    //using use state 
+    const [count, setCount] = useState(0);
     //const tasks = 0;
     //const hideButton =  false
     // const countTasks = () =>
@@ -12,9 +14,11 @@ const CreateToDo = () => {
     //click event function 
     const handleClick = () => {
         tasks ++ ;
+        //utilizing setcount
+        setCount(count + 1);
         console.log("Add Task", tasks);
     }
-    
+
     return (
         <React.Fragment>
         <h1> Tasks : {tasks} </h1>

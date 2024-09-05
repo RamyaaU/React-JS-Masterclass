59648656e30b350bbd81876a6540a5ae175caff3
RamyaU <ramyayadav2016@gmail.com>
2024-09-05 16:01:47 +0530

    created the website layout

---
+++ tasktrek/tasktrek/src/App.jsx
@@ -0,0 +1,35 @@
+// import './App.css';
+// import Card from "./components/Card.jsx";
+// import CreateToDo from './components/CreateToDo.jsx';
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header>
+//         <Card /> 
+//         <CreateToDo />
+//       </header>
+//     </div>
+//   );
+// }
+
+// export default App;
+
+import React from 'react'
+import '../src/App.css'
+
+const App = () => {
+  return (
+    <div className='app'>
+      <header className='app_header'>Header section </header>
+      <main className='app_main'>
+        <section className='task_column'>Section 1</section>
+        <section className='task_column'>Section 2</section>
+        <section className='task_column'>Section 3</section>
+      </main>
+    </div>
+  )
+}
+
+export default App
+

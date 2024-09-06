9a73c92494afb39469859635703b25ab2f3e9011
RamyaU <ramyayadav2016@gmail.com>
2024-09-06 17:22:35 +0530

    css styling

--- tasktrek/tasktrek/src/components/TaskForm.jsx
+++ tasktrek/tasktrek/src/components/TaskForm.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import './TaskForm.css';
 
 const TaskForm = () => {
   return (
@@ -8,16 +9,20 @@ const TaskForm = () => {
         <form>
             <input type='text' className='task_input' placeholder='Enter your task' />
             <div className='task_form_bottom_line'>
+                <div>
                 <button className='tag'>HTML</button>
                 <button className='tag'>CSS</button>
                 <button className='tag'>JS</button>
                 <button className='tag'>React</button>
+                </div>
 
+                <div>
                 <select className='task_status'>
                     <option value='todo'>To Do</option>
                     <option value='doing'>Doing</option>
                     <option value='done'>Done</option>
                 </select>
+                </div>
                 <button type='submit' className='task_submit'> +Add Task</button>
             </div>
         </form>

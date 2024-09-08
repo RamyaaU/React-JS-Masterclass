be28e97cfda493c057cdd71295e807838be0eb71
RamyaU <ramyayadav2016@gmail.com>
2024-09-08 19:22:36 +0530

    another way of implementing props

--- tasktrek/tasktrek/src/components/TaskColumn.jsx
+++ tasktrek/tasktrek/src/components/TaskColumn.jsx
@@ -19,11 +19,15 @@ import React from 'react'
 import Todo from "../assets/direct-hit.png"
 import "./TaskColumn.css"
 
-const TaskColumn = (props) => {
+//old way of init props
+//const TaskColumn = (props) => {
+const TaskColumn = ({title, icon}) => {
   return (
       <section className='task_column'>
         <h2 className='task_column_heading'>
-          <img className='task_column_icon' src={props.icon} alt="To do" /> {props.title}
+          {/* //old way of calling props */}
+          {/* <img className='task_column_icon' src={props} alt="To do" /> {props.title} */}
+          <img className='task_column_icon' src={icon} alt="To do" /> {title}
           To do
         </h2>
       </section>
